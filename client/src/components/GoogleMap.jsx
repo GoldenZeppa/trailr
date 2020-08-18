f9bf942c682d170e10dddc3d86ddb63d7105bb46
Lorie Zepeda <lzepeda.it@gmail.com>
2020-08-18 12:00:10 -0500

    Changes to get app running

--- client/src/components/GoogleMap.jsx
+++ client/src/components/GoogleMap.jsx
@@ -15,7 +15,7 @@ const GoogleMap = React.memo(({ children, ...props }) => (
   <Wrapper>
     <GoogleMapReact
       bootstrapURLKeys={{
-        key: process.env.REACT_APP_MAP_KEY,
+        key: '',
       }}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
